d6c07c3903f95ca71bac959c4fcc932341757d26
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2024-02-02 13:56:56 -0300

    Change description

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
 
   alarm = {
     name                = "account-billing-alarm-${lower(var.currency)}-${var.aws_env}"
-    description         = var.aws_account_id == null ? "Billing consolidated alarm >= ${var.currency} ${var.monthly_billing_threshold}" : "Billing alarm account ${var.aws_account_id} >= ${var.currency} ${var.monthly_billing_threshold}"
+    description         = var.aws_account_id == null ? "Billing consolidated alarm > ${var.currency} ${var.monthly_billing_threshold}" : "Billing alarm account ${var.aws_account_id} > ${var.currency} ${var.monthly_billing_threshold}"
     comparison_operator = "GreaterThanThreshold"
     evaluation_periods  = "1"
     metric_name         = "EstimatedCharges"
